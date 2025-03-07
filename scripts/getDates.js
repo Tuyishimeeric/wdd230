85970ea7f52bb3f5c78213e0c7d5845ef9b69327
Tuyishimeeric <tuyishimeeric001@gmail.com>
2025-03-07 18:18:33 +0200

    week1

---
+++ scripts/getDates.js
@@ -0,0 +1,3 @@
+document.getElementById('currentYear').textContent =new Date().getFullYear();
+const lastmodifiedDate = document.lastmodifiedDate;
+document.getElementById('lastModified').textContent = 'Last modified' + lastmodifiedDateastmodifiedDate;
